3c40f5d265d4752e0fc400f1570a90f3d9aac71b
Laphatize <laphatize@protonmail.com>
2021-12-26 16:26:48 -0500

    Adjust padding

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -277,7 +277,7 @@ const Dashboard = () => {
       <main>
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
 
-          <div className="grid lg:grid-cols-3 gap-4 sm:grid-cols-1">
+          <div className="grid lg:grid-cols-3 gap-10 sm:grid-cols-1">
 
 
 
