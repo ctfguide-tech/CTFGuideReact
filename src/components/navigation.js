942f8f01f91773d28ff5d31b46713f7ed322989b
Pranav Ramesh <laphatize@protonmail.com>
2022-11-26 20:58:55 -0500

    Hide more elements

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,8 +9,8 @@ export class Navigation extends Component {
     render() {
         const navigation = [
             { name: 'Dashboard', href: '../dashboard', current: false },
-            { name: 'Classes', href: '../classes', current: false },
-           // { name: 'Learn', href: '../learn', current: false },
+          //  { name: 'Classes', href: '../classes', current: false },
+            { name: 'Learn', href: '../learn', current: false },
             { name: 'Practice', href: '../practice/all', current: false },
             { name: 'Leaderboards', href: '../leaderboards/global', current: false },
             { name: 'Create', href: '../create', current: false },
@@ -19,7 +19,7 @@ export class Navigation extends Component {
         const navigation2 = [
             { name: 'Dashboard', href: '../dashboard', current: false },
             { name: 'Practice', href: '../practice/all', current: false },
-        //    { name: 'Learn', href: '../learn', current: false },
+            { name: 'Learn', href: '../learn', current: false },
             { name: 'Leaderboards', href: '../leaderboards/global', current: false },
             { name: 'Create', href: '../create', current: false },
         ]
